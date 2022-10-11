402bfd807a0b64435b696f1ce990551571b5fdef
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-10-11 21:28:00 +0700

    Initial Commit

---
+++ Tugas 2 Kalkulator Diskon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tugas 2 Kalkulator Diskon
+//
+//  Created by Erlangga Anugrah Arifin on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

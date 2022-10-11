c61858216e21fe2f2d6bdc0f1d55a7ae01c1b81b
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-10-11 22:56:01 +0700

    final jadi

--- Tugas 2 Kalkulator Diskon/ViewController.swift
+++ Tugas 2 Kalkulator Diskon/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import Toast
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var resultView: UIView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var originalTextField: UITextField!
+    @IBOutlet weak var percentageTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultView.isHidden = true
+        resultLabel.isHidden = true
+        resetButton.isHidden = true
     }
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        if sender.tag == 1 {
+            if originalTextField.text!.isEmpty || percentageTextField.text!.isEmpty {
+                toastError.show()
+            } else {
+                let hargaAsli = Int(originalTextField.text!) ?? 0
+                let persentase = Int(percentageTextField.text!) ?? 0
+                resultLabel.text = "Harga setelah diskon: Rp \(hargaAsli - (hargaAsli * persentase / 100))\nAnda menghemat: Rp \(hargaAsli * persentase / 100)"
+                resultView.isHidden = false
+                resultLabel.isHidden = false
+                resetButton.isHidden = false
+            }
+        } else if sender.tag == 2 { // reset button
+            originalTextField.text = ""
+            percentageTextField.text = ""
+            resultView.isHidden = true
+            resultLabel.isHidden = true
+            resetButton.isHidden = true
+        }
+    }
+    
 
+    let toastError = Toast.default(
+        image: UIImage(systemName: "exclamationmark.triangle")!,
+        title: "Gagal",
+        subtitle: "Lengkapi semua bidang dulu"
+    )
 
 }
 
